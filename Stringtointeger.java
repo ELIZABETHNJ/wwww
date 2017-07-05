392226fc5bd129cba485de20ba39c6865218c11f
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-05 20:52:23 +0530

    Create Stringtointeger.java

---
+++ Stringtointeger.java
@@ -0,0 +1,14 @@
+# wwww
+import java.io.*;
+import java.util.*;
+public class Stringtointeger
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner (System.in);
+    System.out.println("Enter the integer");
+    String x=sc.nextLine();
+    int a=Integer.parseInt(x);
+    System.out.println(a);
+  }
+}
